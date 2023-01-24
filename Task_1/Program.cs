f88d429a859758d6e35793fd56d0ab22a31c8c40
Aleksey <a.y.zayitsev@gmail.com>
2023-01-24 23:58:08 +0300

    натуральные числа в промежутке от N до 1.Рекурсия

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 1)
+{
+    
+    Console.WriteLine("Ввели не натуральное число");
+   return;
+}
+int Nat(int num)
+{
+    if(num == 1)
+    {
+        return 1;
+    }
+    Console.Write($"{num} ");
+    return Nat(num - 1);
+}
+int x = Nat(num);
+Console.WriteLine(x);
+
